24cf03113114ed1800cde0fccea2cf0f5c2d8060
Adam Hynson <hynsondevelops@gmail.com>
2018-05-13 14:35:23 -0400

    Added token based authentication

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module FeedbackApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #adding JWT encode and decode
+    config.autoload_paths << Rails.root.join('lib')
+
   end
 end

86047fa1147809f798dcbe633858dab718c81f3b
Adam Hynson <hynsondevelops@gmail.com>
2018-08-01 01:35:27 -0400

    Added user register error text.

--- feedback-frontend/src/app/home/RegisterComponent.jsx
+++ feedback-frontend/src/app/home/RegisterComponent.jsx
@@ -12,48 +12,89 @@ import PasswordIcon from '@material-ui/icons/EnhancedEncryption';
 
 import Button from '@material-ui/core/Button'
 
-function RegisterComponent({
-	handleRegister,
-}) {
-	return (
-		<div>
-			<h3> Register </h3>
-			<FormControl>
-		        <Input
-		          id="email-register"
-  				  name="email"
-  				  component="input"
-  				  type="email"
-  				  placeholder="Email"
-		          startAdornment={
-		            <InputAdornment position="start">
-		              <AccountCircle />
-		            </InputAdornment>
-		          }
+class RegisterComponent extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {email: "", password: ""}
+	}
+
+
+	handlePassword(e) {
+		this.setState({password: e.target.value})
+	}
+
+	handleLogin(e) {
+		this.setState({email: e.target.value})
+	}
+
+	handleSubmit(e) {
+		if (this.validatePassword() && this.validateEmail()) {
+			this.props.handleRegister(this.state.email, this.state.password)
+		}
+		else {
+			let errors = ""
+			if (!this.validateEmail()) {
+				errors += "\nInvalid email format"
+			}
+			if (!this.validatePassword()) {
+				errors += "\nPassword must be atleast 8 characters"
+			}
+			alert(errors)
+		}
+
+	}
+
+	validateEmail() {
+		return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(this.state.email)
+	}
+
+	validatePassword() {
+		return !(this.state.password == null || this.state.password == "" || this.state.password.length < 8)
+	}
+
+	render() {
+		return (
+			<div>
+				<h3> Register </h3>
+		        <TextField
+		          id="email"
+					  name="email-register"
+					  type="email"
+					  placeholder="Email"
+		          InputProps={{
+	                startAdornment: <InputAdornment position="start">
+	                  <AccountCircle />
+	                </InputAdornment>
+	              }}
+		          onChange={(e) => this.handleLogin(e)}
+		          error={!this.validateEmail()}
+		          helperText={this.validateEmail() ? "" : "Invalid Email Format"}
 		        />
-		     </FormControl>
 		     <br/>
-	    	<FormControl>
-	            <Input
-	              id="password-register"
-  				  name="password"
-  				  component="input"
-  				  type="password"
-  				  placeholder="Password"
-	              startAdornment={
-	                <InputAdornment position="start">
+	            <TextField
+	              id="password"
+					  name="password-register"
+					  type="password"
+					  placeholder="Password"
+					  onChange={(e) => this.handlePassword(e)}
+	              InputProps={{
+	                startAdornment: <InputAdornment position="start">
 	                  <PasswordIcon />
 	                </InputAdornment>
-	              }
+	              }}
+	              error={!this.validatePassword()}
+	              helperText={this.validatePassword() ? "" : "Atleast 8 Characters"}
 	            />
-	         </FormControl>
-	         <div className="buttons">
-	         	<Button onClick={() => handleRegister(document.getElementById("email-register").value, document.getElementById("password-register").value)} variant="contained" component="span">
-	         		Log In
-	         	</Button>
-	         </div>
-		</div>	
-	)
+		         <div className="buttons">
+		         	<Button onClick={() => this.handleSubmit()} variant="contained" component="span">
+		         		Register
+		         	</Button>
+		         </div>
+			</div>	
+		)
+	}
 }
 
 export default RegisterComponent;

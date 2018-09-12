6dbec9849e8bc090fc577d786f1a9132c80ccf01
Adam Hynson <hynsondevelops@gmail.com>
2018-09-12 16:25:21 -0400

    Edited production webpack.

--- feedback-frontend/config/webpack.config.prod.js
+++ feedback-frontend/config/webpack.config.prod.js
@@ -208,7 +208,7 @@ module.exports = {
                     },
                   ],
                 },
-                { test: /\.scss$/, loaders: ['style', 'css', 'sass'], },
+                
                 extractTextPluginOptions
               )
             ),

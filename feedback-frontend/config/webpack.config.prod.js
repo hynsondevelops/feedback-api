b517e534038edee40eae2e72ffba5d84dc129118
Adam Hynson <hynsondevelops@gmail.com>
2018-09-12 17:10:19 -0400

    Edited production webpack.

--- feedback-frontend/config/webpack.config.prod.js
+++ feedback-frontend/config/webpack.config.prod.js
@@ -182,7 +182,7 @@ module.exports = {
                       options: {
                        importLoaders: 1,
                        minimize: true,
-                       modules: true
+                       modules: true,
                        sourceMap: shouldUseSourceMap,
                       },
                     },
@@ -224,7 +224,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.sass$/, /\.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
